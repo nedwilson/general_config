6c6eb4be2e4ac677717298630744d193193650f7
Ned Wilson <nedwilson@gmail.com>
2016-10-24 15:30:03 -0700

    Add CentOS Linux 7 support

---
+++ etc-profile.d-environment.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export IH_SHOW_CODE=darktower
+export IH_SHOW_ROOT=/Volumes/raid_vol01/work/darktower
+export IH_SHOW_CFG_PATH=/Volumes/raid_vol01/work/darktower/SHARED/lib/darktower.cfg

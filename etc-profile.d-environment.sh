f55676fdc0a888bba42bbf25077ac4b46ac91e1d
Ned Wilson <ned@glenrothes.n3d.org>
2017-11-15 11:20:28 -0800

    Add environment variables for PYTHONPATH and a few others.

--- etc-profile.d-environment.sh
+++ etc-profile.d-environment.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-export IH_SHOW_CODE=darktower
-export IH_SHOW_ROOT=/Volumes/raid_vol01/work/darktower
-export IH_SHOW_CFG_PATH=/Volumes/raid_vol01/work/darktower/SHARED/lib/darktower.cfg
+export IH_SHOW_CODE=spinel
+export IH_SHOW_ROOT=/Volumes/raid_vol01/shows/spinel
+export IH_SHOW_CFG_PATH=/Volumes/raid_vol01/shows/spinel/SHARED/lib/spinel.cfg
+export IH_SHOW_CFG_LUT=/Volumes/raid_vol01/shows/spinel/SHARED/lut/FotokemSpinel_showLUT.cube
+export PYTHONPATH=/Volumes/raid_vol01/shows/SHARED/lib/python
+export NUKE_INTERACTIVE=1
+export FN_DISABLE_LICENSE_DIALOG=TRUE
